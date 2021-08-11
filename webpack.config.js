2647e5a9b81853c1b0f5ab360deb4b8a832855fb
Cristian Vega <cris.vega.03@gmail.com>
2021-08-10 19:37:03 -0500

    header image fixed

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,17 @@ module.exports = {
           'sass-loader',
         ],
       },
+      {
+        test: /\.(png|gif|jpg|svg)$/,
+        use: [
+          {
+            'loader': 'file-loader',
+            'options': {
+              name: 'assets/[hash].[ext]',
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
