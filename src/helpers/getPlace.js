c418057ba55b5e5c77f17fed3eb14976d243a8e0
Cristian Vega <cris.vega.03@gmail.com>
2021-11-05 13:19:28 -0500

    key update

--- src/helpers/getPlace.js
+++ src/helpers/getPlace.js
@@ -5,15 +5,15 @@ export const getPlace = async( dir ) => {
   // API opencagedata TO GET COORDS
 
   const encodeUrl = encodeURI( dir );
-  
-  const API_PLACE = `https://api.opencagedata.com/geocode/v1/json?q=${encodeUrl}&key=e46937d4c6ed411fac30c588096e4221`;
-  
+
+  const API_PLACE = `https://api.opencagedata.com/geocode/v1/json?q=${encodeUrl}&key=103300d7d33140e38c0a5ecb64efcbe0`;
+
   const resp = await axios.get(API_PLACE);
 
   if (resp.data.results.length === 0) {
     throw new Error(`No hay resultados para ${ dir }`);
   }
-  
+
   const id = new Date().getTime();
   const { country, city: location }  = resp.data.results[0].components;
   const { lat, lng } = resp.data.results[0].geometry
@@ -24,6 +24,6 @@ export const getPlace = async( dir ) => {
     location,
     lat,
     lng
-  }  
+  }
 
 };
